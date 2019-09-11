29a91e50161227943126b7eed330b28aaac439a9
Anh Tu Nguyen <anhtu293@gmail.com>
2019-09-11 22:42:09 +0200

    data augmentation completed

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## 1. Data analysis
 
+There is obviously imbalanced phenomenon in this data set : Some classes have only 2 or 3 examples, others have up to 10 examples. To solve this problem, I find classes which have less than 5 examples and do some random transformations : with classes which have 2 examples, 4 transformations were implemented, with classes which have 3 examples, 3 transformations were implemented and with classes which have 4 examples, 2 transformations were implemented. Therefor, all the classes basically have at least 6 example for training. 
+
+With the limited number of data, the idea is to use a pretrained model for face recognition. After having done some research, I found a pretrained model called FaceNet that would be useful. The limitation of this model is that the data is not faces of asian people meanwhile our purpose is to recognize vietnamese celebrities. 
+
 ## 2. Data preprocessing
 
 ## 3. Models 
--- data_generator.py
+++ data_generator.py
@@ -2,19 +2,19 @@ import numpy as np
 from skimage import io 
 import pandas as pd
 from skimage import transform
-from skimage import utils
+from skimage import util
 import random
 import csv
 
 def load_label(filepath):
 	return pd.read_csv(filepath)
 
-def rotation(img):
+def rotate(img):
 	degree = random.uniform(-45,45)
 	return transform.rotate(img, degree)
 
 def add_noise(img):
-	return utils.random_noise(img)
+	return util.random_noise(img)
 
 def flip(img):
 	return img[:,::-1]
@@ -22,35 +22,52 @@ def flip(img):
 data_augmentation = {'rotate' : rotate, 'noise' : add_noise, 'flip' : flip}
 
 def label_frequency(label):
-	frequency_table = pd.crosstab(index = label[1], columns = "count")
+	frequency_table = pd.crosstab(index = label["label"], columns = "count")
 	return frequency_table
 
-def data_augmentation():
-	print("==========Start Data Augemntation==========\n")
-	filepath = "./data/train.csv"
-	label = load_label(filepath)
+print("==========Start Data Augemntation==========\n")
+filepath = "./data/train.csv"
+label = load_label(filepath)
 
-	#get frequency_table
-	frequency_table = label_frequency(label)
+#get frequency_table
+frequency_table = label_frequency(label)
 	
-	#find imbalanced classes
-	less_than_3 = frequency_table["count"] < 3 
-	classes = np.asarray((table[less_than_3].index)).astype(int)
+#find imbalanced classes
+less_than_3 = frequency_table["count"] < 5 
+classes = np.asarray((frequency_table[less_than_3].index)).astype(int)
 	
-	for i in range(classes):
+name_class = []
+
+for i in range(classes.shape[0]):
+	if frequency_table["count"][classes[i]] == 2:
+		transformations = data_augmentation
+	if frequency_table["count"][classes[i]] == 3:
+		transformations = random.sample(list(data_augmentation.keys()),3)
+	if frequency_table["count"][classes[i]] == 4:
 		transformations = random.sample(list(data_augmentation.keys()),2)
-		
-		#find path of imgs to be transformed
-		class_to_transform = label[1] == classes[i]
-		pathes = label[0][class_to_transform]
 
-		path_file = "./data/train/"
-		for path in pathes:
-			img_path = path_file + path
-			img = io.imread(img_path)
-			for trans in transformations:
-				transformed_img = data_augmentation[trans](img)
-				new_file_path = img_path[:-4] + trans + ".png"
-				io.imsave(new_file_path, transformed_img)
+	transformations = random.sample(list(data_augmentation.keys()),2)
+	
+	#find path of imgs to be transformed
+	class_to_transform = label["label"] == classes[i]
+	paths = label["image"][class_to_transform]
 
-	print("Data Augmentation Completed !")
+	path_file = "./data/train/"
+	for path in paths:
+		img_path = path_file + path
+		img = io.imread(img_path)
+		for trans in transformations:
+			transformed_img = data_augmentation[trans](img)
+			new_file_path = img_path[:-4] + "_" + trans + ".png"
+			io.imsave(new_file_path, transformed_img)
+
+			name_image = path[:-4] + "_" + trans + ".png"
+			name_file = [name_image, classes[i]]
+			name_class.append(name_file)
+
+#add index to label file
+with open(filepath, 'a') as file:
+	writer = csv.writer(file)
+	writer.writerows(name_class)
+
+print("Data Augmentation Completed !")
