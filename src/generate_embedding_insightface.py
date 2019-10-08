4a2f307855544cc8deaf0e0b8ab20afcd5827a1e
Anh Tu Nguyen <anhtu293@gmail.com>
2019-10-08 22:18:05 +0200

    add embeddings insight

--- src/generate_embedding_insightface.py
+++ src/generate_embedding_insightface.py
@@ -24,7 +24,7 @@ data_path  = '../data/train_112x112'
 if __name__ == '__main__':
     model = face_model.FaceModel(args)
     img_file = []
-    output_dir = './embeddings/insightface/{}/{}'.format(data_path.split("/")[2], args.model.split("/")[3])
+    output_dir = './embeddings/insight/{}/{}'.format(data_path.split("/")[2], args.model.split("/")[3])
     labels = pd.read_csv("../data/train.csv")
     print(labels.head())
 
@@ -32,19 +32,21 @@ if __name__ == '__main__':
         img_path = data_path + "/" + labels["image"][i]
         if not os.path.exists(img_path):
             continue
-        img = cv2.imread(img_path)
+        img_origin = cv2.imread(img_path)
         print("{}/{} : {}".format(i, len(labels), img_path))
 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_origin = cv2.cvtColor(img_origin, cv2.COLOR_BGR2RGB)
+        img = np.transpose(img_origin, (2,0,1))
         embeddings = model.get_feature(img)
-        np.save(output_dir + '%s.npy'%labels["image"][i][:-4], embeddings)
+        np.save(output_dir + '/%s.npy'%labels["image"][i][:-4], embeddings)
         img_file.append([output_dir + '/%s.npy'%labels["image"][i][:-4], labels['label'][i]])
 
-        img_flip = cv2.flip(img, 1)
+        img_flip = cv2.flip(img_origin, 1)
+        img_flip = np.transpose(img_flip, (2,0,1))
         embeddings = model.get_feature(img_flip)
-        np.save(output_dir + '%s_flip.npy'%labels["image"][i][:-4], embeddings)
+        np.save(output_dir + '/%s_flip.npy'%labels["image"][i][:-4], embeddings)
         img_file.append([output_dir + "/" + labels["image"][i][:-4] + "_flip.npy", labels['label'][i]])
     
-    with open("./embeddings/insightface/embs_class_{}_{}.csv".format(data_path[8:], args.model.split("/")[3]), 'a') as file:
+    with open("./embeddings/insight/embs_class_{}_{}.csv".format(data_path[8:], args.model.split("/")[3]), 'a') as file:
         writer = csv.writer(file)
         writer.writerows(img_file)
