290f92594ffd1ff21f2828c67c62f6849dbfed7f
Anh Tu Nguyen <anhtu293@gmail.com>
2019-10-09 15:00:18 +0200

    add train classifier

---
+++ src/face_alignment_test.py
@@ -0,0 +1,121 @@
+import numpy as np
+from skimage import io
+import cv2
+from skimage.transform import resize
+from skimage import transform as trans
+import pandas as pd
+import time
+from matplotlib import pyplot as plt
+import sys
+from face_detector import SSHDetector
+import os
+sys.path.append("../backbones/mtcnn")
+from mtcnn_detector import MtcnnDetector
+
+
+def face_alignment(img, size, bbox = None, landmark = None, margin = 15):
+	if landmark is not None:
+		if size == 112:
+			src = np.array([
+				[38.2946, 51.6963],
+				[73.5318, 51.5014],
+				[56.0252, 71.7366],
+				[41.5493, 92.3655],
+				[70.7299, 92.2041]], dtype = np.float32)
+		elif size == 160:
+			src = np.array([
+				[54.706573, 73.85186],
+				[105.045425, 73.573425],
+				[80.036, 102.48086],
+				[59.356144, 131.95071],
+				[101.04271, 131.72014]], dtype = np.float32)
+		dst = np.asarray(landmark).astype(np.float32)
+		tform = trans.SimilarityTransform()
+		tform.estimate(dst, src)
+		M = tform.params[0:2,:]
+
+	if M is None:
+		if bbox is None:  # use center crop
+			det = np.zeros(4, dtype=np.int32)
+			det[0] = int(img.shape[1] * 0.0625)
+			det[1] = int(img.shape[0] * 0.0625)
+			det[2] = img.shape[1] - det[0]
+			det[3] = img.shape[0] - det[1]
+		else:
+			det = bbox
+		bb = np.zeros(4, dtype=np.int32)
+		bb[0] = np.maximum(det[0] - margin / 2, 0)
+		bb[1] = np.maximum(det[1] - margin / 2, 0)
+		bb[2] = np.minimum(det[2] + margin / 2, img.shape[1])
+		bb[3] = np.minimum(det[3] + margin / 2, img.shape[0])
+		ret = img[bb[1]:bb[3], bb[0]:bb[2], :]
+		ret = cv2.resize(ret, (size, size))
+		return ret
+	else:
+		warped = cv2.warpAffine(img, M, (size,size), borderValue = 0)
+		return warped
+
+def get_scales(img):
+    TEST_SCALES = [100, 200, 300, 400]
+    target_size = 400
+    max_size = 1200
+    im_shape = img.shape
+    im_size_min = np.min(im_shape[0:2])
+    im_size_max = np.max(im_shape[0:2])
+    im_scale = float(target_size) / float(im_size_min)
+    # prevent bigger axis from being more than max_size:
+    if np.round(im_scale * im_size_max) > max_size:
+        im_scale = float(max_size) / float(im_size_max)
+    scales = [float(scale) / target_size * im_scale for scale in TEST_SCALES]
+    return scales
+
+if __name__ == '__main__':
+	#delete file in test repo
+	listfile = os.listdir('../data/test_112x112')
+	if len(listfile) > 0:
+		os.system('rm ../data/test_112x112/*')
+	listfile = os.listdir('../data/test_160x160')
+	if len(listfile) > 0:
+		os.system('rm ../data/test_160x160/*')
+	listfile = os.listdir('../data/test_unknown')
+	if len(listfile) > 0:
+		os.system('rm ../data/test_unknown/*')
+
+	print("Start Face Alignment\n")
+	labels = os.listdir("../data/test/")
+	labels = pd.DataFrame(labels)
+	labels.columns = ["image"]
+	print(len(labels))
+	print(labels.head(5))
+	face_detector = SSHDetector(prefix = "../models/ssh/sshb", ctx_id= -1, epoch = 0, test_mode = True)
+	landmark_detector = MtcnnDetector(model_folder = "../backbones/mtcnn/model")
+	
+	#test = pd.DataFrame(columns = ["image", "class"])
+	
+	for i in range(labels.shape[0]):
+		print("Image : {} ----- File : {} ------\n".format(i+1, labels['image'][i]))
+		img = io.imread('../data/test/' + labels['image'][i])
+		scales = get_scales(img)
+		results = face_detector.detect(img, scales = scales, threshold = 0.2)
+		if len(results) != 0:
+			landmarks = landmark_detector.get_landmark(img, results)
+			if landmarks != None:
+				_, points = landmarks
+				points_array = points[0,:].reshape((2,5)).T
+				new_face_112x112 = face_alignment(img, 112, landmark = points_array, bbox = results[0])
+				new_face_160x160 = face_alignment(img, 160, landmark = points_array, bbox = results[0])
+				
+				io.imsave('../data/test_112x112/'+labels['image'][i], new_face_112x112)
+				io.imsave('../data/test_160x160/'+labels['image'][i], new_face_160x160)
+				#train.append(labels[i])
+			else:
+				print("Cannot detect !")
+				io.imsave('../data/test_unknown/'+labels['image'][i], img)
+		else:
+			print("Cannot detect !")
+			io.imsave('../data/test_unknown/'+labels['image'][i], img)
+	#train.to_csv("../data/train_embedding.csv")
+	labels.to_csv("../data/test.csv")
+	print("Face alignment completed !")
+
+
--- src/train_classifier_facenet.py
+++ src/train_classifier_facenet.py
@@ -8,18 +8,18 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Input, Flatten, Dropout, add
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
-from tensorflow.keras.utils.np_utils import to_categorical
+from tensorflow.keras.utils import to_categorical
 
 import argparse
 
 parser = argparse.ArgumentParser(description = "train classifier of facenet model")
 
 parser.add_argument("--model", default = "20180402-114759", help = "model name")
-parser.add_argument("--embeddings", default = "./embeddings/facenet/embs_class_train_160x160_20180402-114759.npy", help = "path to file index embeddings for train")
+parser.add_argument("--embeddings", default = "./embeddings/facenet/embs_class_train_160x160_20180402-114759.csv", help = "path to file index embeddings for train")
 parser.add_argument("--size", default = 160, help = 'image size')
 args = parser.parse_args()
 
-INPUT_SHAPE = 128
+INPUT_SHAPE = 512
 LEARNING_RATE = 0.001
 BATCH_SIZE = 64
 
@@ -33,15 +33,16 @@ def build_model():
 def DataGenerator(labels, classes, batch_size):
     i = 0
     while True:
-        embeddings_batch = []
+        embeddings_batch = np.array([],dtype = np.float32).reshape(0,512)
         class_batch = []
         for b in range(batch_size):
             if i == labels.shape[0]:
                 i = 0
-            sample = np.load(labels[0][i])
+            sample = np.load(labels["emb"][i]).reshape(-1,512)
             cl = classes[i]
-            embeddings_batch.append(sample)
+            embeddings_batch = np.vstack((embeddings_batch, sample))
             class_batch.append(cl)
+            i += 1
         yield np.array(embeddings_batch), np.array(class_batch)
 
 def convert2categorical(labels):
@@ -53,24 +54,34 @@ def convert2categorical(labels):
 if __name__ == '__main__':
     model = build_model()
     labels = pd.read_csv(args.embeddings)
+    labels.columns = ['emb', 'class']
+    print(labels.head)
+    categories = convert2categorical(labels)
+    """
     labels_train = labels.sample(frac = 0.8, random_state = 0)
     labels_val = labels.drop(labels_train.index)
     categories_train = convert2categorical(labels_train)
     categories_val = convert2categorical(labels_val)
+    
+    labels_train.reset_index(inplace = True)
+    labels_val.reset_index(inplace = True)
+    print(labels_train.head)
+    print(labels_val.head)
+    print(categories_train.shape)
+    print(categories_val.shape)
+    """
 
-    train_generator = DataGenerator(labels_train, categories_train, BATCH_SIZE)
-    val_generator = DataGenerator(labels_val, categories_val, BATCH_SIZE)
+    train_generator = DataGenerator(labels, categories, BATCH_SIZE)
+    #val_generator = DataGenerator(labels_val, categories_val, BATCH_SIZE)
 
     optimizer = Adam(LEARNING_RATE)
     metric = tf.keras.metrics.CategoricalAccuracy()
-    model.compile(optimizer = optimizer, loss = 'sparse_categorical_crossentropy', metrics = [metric])
+    model.compile(optimizer = optimizer, loss = 'categorical_crossentropy', metrics = [metric])
 
     callbacks = [
 		tf.keras.callbacks.EarlyStopping(patience = 8, monitor = 'val_acc', restore_best_weights = True),
-		tf.keras.callbacks.ModelCheckpoint(filepath ="./checkpoints/"+args.model+"/weights-epoch{epoch:02d}-loss{val_loss:.2f}.h5")
+		tf.keras.callbacks.ModelCheckpoint(filepath ="./checkpoints/facenet/"+args.model+"/weights-epoch{epoch:02d}-loss{val_loss:.2f}.h5")
 		]
-    history = model.fit_generator(generator = train_generator, epochs = 100, verbose = 1,
-        validation_data = val_generator, steps_per_epoch = len(labels_train)/BATCH_SIZE,
-        validation_steps = len(labels_val)/BATCH_SIZE)
+    history = model.fit_generator(generator = train_generator, epochs = 100, verbose = 1, steps_per_epoch = len(labels)/BATCH_SIZE)
     
-    model.save("./model/facenet/" + args.model + "/model_" + args.model + "_" + args.size + ".h5")
+    model.save("./models/facenet//model_" + args.model + "_" + str(args.size) + ".h5")
